84c9bdfeda2973fd80322799abad969f240dedf8
Saverof <110932378+Saverof@users.noreply.github.com>
2022-10-06 10:15:50 +0300

    Add files via upload

---
+++ Solve_one/Solve_one/Solve_one.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <iterator>
+using namespace std;
+
+void solve(string& str, int max_size)
+{
+    // Уаляем все пробелы больше одного
+    str.erase(unique_copy(str.begin(), str.end(), str.begin(), [](char ch1, char ch2) { return ch1 == ' ' && ch2 == ' '; }), str.end());
+
+    // Проверяем чем является первый символ
+    if (str.at(0) == ' ')
+    {
+        str.erase(0, 1);
+    }
+
+    // Если размер больше, уменьшаем размер
+    if (str.size() > max_size)
+    {
+        str.erase(max_size, max_size + str.size());
+        str.append("...");
+    }
+}
+
+int main()
+{
+    // Задаем исходные данные
+    int m = 40;
+    string text = "   Lorem   ipsum  dolor      sit  amet,    consectetuer adipiscing elit.       Maecenas porttitor congue   massa. Fusce posuere, magna sed  pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis     urna.";
+   
+    // Вызываем решение
+    solve(text, m);
+
+    // Выводим
+    cout << text << '\n';
+
+    return 0;
+}
---
+++ Solve_two/Solve_two/Solve_two.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void bracket_check(string str)
+{
+    bool state = true;  // условие выхода из цикла state == false
+    bool stateEND = false; // принудительное окончание цикла
+    int chL = 0; // счет левых скобок (
+    int chR = 0; // счет правых скобок )
+    int i = 0; // итератор внутри цикла
+
+
+    while (state)
+    {
+        switch (str[i])
+        {
+        case '(': 
+            chL++;
+            break;
+        case ')':
+            chR++;
+            break;
+        default:
+            break;
+        }
+
+        if (i == str.size())
+        {
+            state = false;
+            stateEND = true;
+        }
+
+        i++;
+
+        if (chL == chR)
+        {
+            if (i == str.size())
+            {
+                state = false;
+            }
+        }
+    }
+
+    if (i == str.size() && stateEND != true)
+    {
+        cout << str << " - правильная скобочная последовательность\n";
+    }
+    else
+    {
+        cout << str << " - неправильная скобочная последовательность\n";
+    }
+   
+    
+}
+
+int main() 
+{
+    setlocale(LC_ALL, "Rus");
+    bracket_check("((x * y) + (2 * (x + y))) * (y + 3)");
+    bracket_check("((((x * y) + (2 * (x - y)))");
+
+}
+
