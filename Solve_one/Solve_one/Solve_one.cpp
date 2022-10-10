2f3282c0479cefc0a35ec358b0498083dcb1f81a
Saverof <110932378+Saverof@users.noreply.github.com>
2022-10-11 01:07:15 +0300

    Add files via upload

--- Solve_one/Solve_one/Solve_one.cpp
+++ Solve_one/Solve_one/Solve_one.cpp
@@ -1,38 +1,43 @@
 ﻿#include <iostream>
 #include <algorithm>
-#include <iterator>
+
 using namespace std;
 
-void solve(string& str, int max_size)
+string solve(string str, int max_size)
 {
-    // Уаляем все пробелы больше одного
-    str.erase(unique_copy(str.begin(), str.end(), str.begin(), [](char ch1, char ch2) { return ch1 == ' ' && ch2 == ' '; }), str.end());
+    // Переменная для хранения обработанного текста
+    string new_text = str;
+
+    // Удаляем все пробелы больше одного
+    new_text.erase(unique_copy(new_text.begin(), new_text.end(), new_text.begin(), [](char ch1, char ch2) { return ch1 == ' ' && ch2 == ' '; }), new_text.end());
 
     // Проверяем чем является первый символ
-    if (str.at(0) == ' ')
+    if (new_text.at(0) == ' ')
+        new_text.erase(0, 1);
+    
+    // Проверяем чем является последний символ
+    if (new_text.back() == ' ')
+        new_text.pop_back();
+   
+    // Если размер больше максимального значения, уменьшаем размер
+    if (new_text.size() > max_size)
     {
-        str.erase(0, 1);
+        new_text.erase(max_size, max_size + new_text.size());
+        new_text.append("...");
     }
 
-    // Если размер больше, уменьшаем размер
-    if (str.size() > max_size)
-    {
-        str.erase(max_size, max_size + str.size());
-        str.append("...");
-    }
+    return new_text;
 }
 
 int main()
 {
     // Задаем исходные данные
     int m = 40;
-    string text = "   Lorem   ipsum  dolor      sit  amet,    consectetuer adipiscing elit.       Maecenas porttitor congue   massa. Fusce posuere, magna sed  pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis     urna.";
-   
-    // Вызываем решение
-    solve(text, m);
+    string text = "   Lorem   ipsum  dolor      sit  amet,    consectetuer adipiscing elit.       Maecenas porttitor congue   massa. Fusce posuere, magna sed  pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis     urna.      ";
 
-    // Выводим
+    // Выводим исходную строку
     cout << text << '\n';
-
+    // Выводим исправленную строку
+    cout << endl << solve(text, m) << '\n';
     return 0;
 }
